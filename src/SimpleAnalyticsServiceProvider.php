5d1b69291c90215e51f467fc7a368b920483dcf8
Manuel Roth <manuel.roth@gmail.com>
2022-09-11 16:19:01 +0200

    fix

--- src/SimpleAnalyticsServiceProvider.php
+++ src/SimpleAnalyticsServiceProvider.php
@@ -20,7 +20,7 @@ class SimpleAnalyticsServiceProvider extends ServiceProvider
             __DIR__ . '/config/config.php' => config_path('simple-analytics.php'),
         ], 'config');
 
-        $this->loadViewsFrom(__DIR__ . '/resources/views', 'simple-analytics');
+        $this->loadViewsFrom(__DIR__ . '/resources/views', 'SimpleAnalytics');
     }
 
     private function getAutoEvents () {
