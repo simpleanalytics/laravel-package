ececa692ad87348e78862e0b90be569083a3aa50
Manuel Roth <manuel.roth@gmail.com>
2022-09-13 11:52:40 +0200

    fix ui

--- src/resources/views/scripts.blade.php
+++ src/resources/views/scripts.blade.php
@@ -1,8 +1,8 @@
-@if ($enabled))
+@if ($enabled)
     <!-- SimpleAnalytics -->
     <script {!!$settings!!} async defer src="https://{{$domain}}/{{$auto_events === true ? 'plus' : 'latest'}}.js"></script>
 
-    @if ($auto_events))
+    @if ($auto_events)
         <script {!!$settings_events!!} async src="https://scripts.simpleanalyticscdn.com/auto-events.js"></script>
     @endif
     <noscript><img src="https://queue.simpleanalyticscdn.com/noscript.gif" alt="" referrerpolicy="no-referrer-when-downgrade" /></noscript>
