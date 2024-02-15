6829bc94e617de7c09d2b5391afeb3c62e08bbbe
Manuel Roth <manuel.roth@gmail.com>
2024-02-15 12:01:39 +0100

    add alt to noscript

--- src/resources/views/scripts.blade.php
+++ src/resources/views/scripts.blade.php
@@ -5,7 +5,7 @@
     @if ($auto_events)
         <script {!!$settings_events!!} async src="https://scripts.simpleanalyticscdn.com/auto-events.js"></script>
     @endif
-    <noscript><img src="https://queue.simpleanalyticscdn.com/noscript.gif" alt="" referrerpolicy="no-referrer-when-downgrade" /></noscript>
+    <noscript><img src="https://queue.simpleanalyticscdn.com/noscript.gif" alt="NoScript" referrerpolicy="no-referrer-when-downgrade" /></noscript>
 @endif
 
 
